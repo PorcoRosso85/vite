ff1c1d1f13f01d4a8c89136fbe952d433dac9431
PorcoRosso85 <1.is.universe@gmail.com>
2024-09-22 17:16:21 +0900

    removed tailwind n postcss

--- jsx/src/App.tsx
+++ jsx/src/App.tsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+const headerStyle = {
+  fontSize: '1rem', // text-1xl
+  fontWeight: 'bold',  // font-bold
+  textDecoration: 'underline', // underline
+};
+
 function App() {
   const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
+        Edit <code>src/App.tsx</code> and save to test HMR
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,15 +25,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-3xl font-bold underline'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <p style={headerStyle}>Vite + React</p>
+      <div className="card"></div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- jsx/src/main.tsx
+++ jsx/src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './main.css'
 import { PgLite } from './PgLite.tsx'
 
 createRoot(document.getElementById('main')!).render(
   <StrictMode>
-    {/* <App /> */}
+    <h3>App.tsx</h3>
+    <App />
+    <br />
+    <h3>PgLite.tsx</h3>
     <PgLite />
   </StrictMode>,
 )
