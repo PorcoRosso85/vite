a25bff2aa9d8816a035aa5a64d984345a9dde9d5
PorcoRosso85 <1.is.universe@gmail.com>
2024-09-23 11:12:01 +0900

    remove test for 'sqlc'

--- jsx/src/features/wyah/repositories/prismaGenRepository.test.ts
+++ jsx/src/features/wyah/repositories/prismaGenRepository.test.ts
@@ -1,35 +1,35 @@
-import fs from "fs";
-import path from "path";
-import { PGlite } from "@electric-sql/pglite";
-import { afterEach, beforeEach, describe, expect, test } from "vitest";
-import { getUser } from "./query_sql";
+// import fs from "fs";
+// import path from "path";
+// import { PGlite } from "@electric-sql/pglite";
+// import { afterEach, beforeEach, describe, expect, test } from "vitest";
+// import { getUser } from "./query_sql";
 
-describe("user", async () => {
-	const db = await PGlite.create();
+// describe("user", async () => {
+// 	const db = await PGlite.create();
 
-	beforeEach(async () => {
-		// データベースへの ping (接続テスト)
-		await db.exec("SELECT 1");
-		console.log("Connected to the database");
+// 	beforeEach(async () => {
+// 		// データベースへの ping (接続テスト)
+// 		await db.exec("SELECT 1");
+// 		console.log("Connected to the database");
 
-		// ファイルを読み込んでSQL文を取得, スキーマの初期化
-		const sqlFilePath = path.resolve(__dirname, "./migrations/schema.sql");
-		const schemaSQL = fs.readFileSync(sqlFilePath, "utf-8");
-		await db.exec(schemaSQL);
-		console.log("Schema initialized");
-	});
+// 		// ファイルを読み込んでSQL文を取得, スキーマの初期化
+// 		const sqlFilePath = path.resolve(__dirname, "./migrations/schema.sql");
+// 		const schemaSQL = fs.readFileSync(sqlFilePath, "utf-8");
+// 		await db.exec(schemaSQL);
+// 		console.log("Schema initialized");
+// 	});
 
-	afterEach(async () => { });
+// 	afterEach(async () => { });
 
-	test("create", async () => {
-		const tables = await db.exec(
-			"select table_name from information_schema.tables where table_schema = 'public'",
-		);
-		// console.log("tables", JSON.stringify(tables, null, 2));
-		await db.exec(
-			`insert into "user" (id, username, email) values ('1', 'test', 'test@example.com')`,
-		);
-		const result = await getUser(db)
-		console.log(result);
-	});
-});
+// 	test("create", async () => {
+// 		const tables = await db.exec(
+// 			"select table_name from information_schema.tables where table_schema = 'public'",
+// 		);
+// 		// console.log("tables", JSON.stringify(tables, null, 2));
+// 		await db.exec(
+// 			`insert into "user" (id, username, email) values ('1', 'test', 'test@example.com')`,
+// 		);
+// 		const result = await getUser(db)
+// 		console.log(result);
+// 	});
+// });
